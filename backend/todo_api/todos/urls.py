0405c440a8e6adcb40f8eb27f22846efe35cd7fa
NatalmanNah <87883622+NatalmanNah@users.noreply.github.com>
2022-02-13 19:00:41 -0600

    all file added

---
+++ backend/todo_api/todos/urls.py
@@ -0,0 +1,10 @@
+# todos/urls.py
+# todos/urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.ListTodo.as_view()),
+    path('<int:pk>/', views.DetailTodo.as_view()),
+]
